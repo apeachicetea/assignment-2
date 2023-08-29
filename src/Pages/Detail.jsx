05632d8fa10372e906fac9060540f2f4aa33a1ca
apeachicetea <eodbszla@gmail.com>
2023-08-29 21:43:11 +0900

    feat: 디테일 기본 구조 작성

--- src/Pages/Detail.jsx
+++ src/Pages/Detail.jsx
@@ -8,11 +8,37 @@ function Detail() {
 
   const [issue, setIssue] = useState({});
 
+  console.log(issue);
+
   useEffect(() => {
     getAnIssue(issueId).then((issue) => setIssue(issue));
   }, []);
 
-  return <></>;
+  return (
+    <>
+      {Object.keys(issue).length && (
+        <>
+          <div>
+            <img src={issue.user.avatar_url} alt="avatar_img" />
+          </div>
+          <div>
+            <span>#{issue.number}</span>
+            <span>{issue.title}</span>
+            <div>
+              <div>작성자: {issue.user.login}</div>
+              <div>작성일: {issue.created_at}</div>
+            </div>
+          </div>
+          <div>
+            <div>
+              <div>Comment: {issue.comments}</div>
+            </div>
+          </div>
+          <div>{issue.body}</div>
+        </>
+      )}
+    </>
+  );
 }
 
 export default Detail;
