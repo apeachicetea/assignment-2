47cf6774e777f00eb5a6e572c23289027105b3ba
apeachicetea <eodbszla@gmail.com>
2023-08-29 19:27:29 +0900

    feat: 전체 이슈 페이지 작성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import { Fragment } from "react";
+import Issues from "./Pages/Issues";
 
 function App() {
-  return <Fragment></Fragment>;
+  return (
+    <Fragment>
+      <Issues />
+    </Fragment>
+  );
 }
 
 export default App;
--- src/Pages/Issues.jsx
+++ src/Pages/Issues.jsx
@@ -1,5 +1,44 @@
+import { useEffect, useState } from "react";
+import { styled } from "styled-components";
+import { getIssues } from "../Utils/IssuesUtil";
+
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
 function Issues() {
-  return;
+  const [issues, setIssues] = useState([]);
+
+  useEffect(() => {
+    getIssues().then((issues) => setIssues(issues));
+  }, []);
+
+  return (
+    <Container>
+      {issues.slice(0, 10).map((issue) => {
+        return (
+          <ul key={issue.id}>
+            <div>
+              <span>{issue.number}</span>
+              <span>{issue.title}</span>
+            </div>
+            <div>
+              <div>{issue.user.login}</div>
+              <div>{issue.created_at}</div>
+            </div>
+            <div>
+              <div>{issue.comments}</div>
+            </div>
+          </ul>
+        );
+      })}
+    </Container>
+  );
 }
 
 export default Issues;
