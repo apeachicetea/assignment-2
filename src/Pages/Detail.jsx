bd14d02187a1a89cacd93402dc1fe2475be835dd
apeachicetea <eodbszla@gmail.com>
2023-08-30 19:12:28 +0900

    feat: 디테일 페이지 스타일링

--- src/Pages/Detail.jsx
+++ src/Pages/Detail.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
@@ -8,12 +8,46 @@ import { getAnIssueThunk } from "../Redux/IssueSlice";
 import Header from "../Components/Header";
 import { styled } from "styled-components";
 
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  padding: 30px;
+`;
+
+const UpWrapper = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+`;
+
+const TitleWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: solid 1px black;
+  padding: 0px 10px 10px 10px;
+  margin-left: 10px;
+`;
+
 const Wrapper = styled.div`
   width: 100vw;
   height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 10px;
+`;
+
+const Author = styled.span`
+  margin-right: 5px;
+`;
+
+const AvatarImg = styled.img`
+  width: 50px;
+  height: 50px;
 `;
 
 function Detail() {
@@ -32,25 +66,29 @@ function Detail() {
     <>
       <Header />
       {Object.keys(issue).length ? (
-        <>
-          <div>
-            <img src={issue.user.avatar_url} alt="avatar_img" />
-          </div>
-          <div>
-            <span>#{issue.number}</span>
-            <span>{issue.title}</span>
+        <Container>
+          <UpWrapper>
             <div>
-              <div>작성자: {issue.user.login}</div>
-              <div>작성일: {issue.created_at}</div>
+              <AvatarImg src={issue.user.avatar_url} alt="avatar_img" />
             </div>
-          </div>
-          <div>
-            <div>
-              <div>Comment: {issue.comments}</div>
-            </div>
-          </div>
+            <TitleWrapper>
+              <div>
+                <Title>
+                  <span>#{issue.number}</span>
+                  <span>{issue.title}</span>
+                </Title>
+                <div>
+                  <Author>작성자: {issue.user.login}</Author>
+                  <span>작성일: {issue.created_at}</span>
+                </div>
+              </div>
+              <div>
+                <div>Comment: {issue.comments}</div>
+              </div>
+            </TitleWrapper>
+          </UpWrapper>
           <ReactMarkdown children={issue.body} remarkPlugins={[remarkGfm]} />
-        </>
+        </Container>
       ) : (
         <Wrapper>
           <LoadingIndicator />
